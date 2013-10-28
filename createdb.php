a19810ec639ba35e6e6f7f29abd2ecee1b5ae282
Vlad Vovk <zerocool0507@gmail.com>
2013-10-28 19:56:10 +0200

    add createdb

---
+++ createdb.php
@@ -0,0 +1,25 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_LOGIN', 'root');
+define('DB_PASSWORD', 'qwert');
+
+mysql_connect(DB_HOST, DB_LOGIN, DB_PASSWORD) or die(mysql_error());
+
+$sql = 'CREATE DATABASE gbook';
+mysql_query($sql) or die(mysql_error());
+mysql_select_db('gbook') or die(mysql_error());
+$sql = "
+	CREATE TABLE get (
+		id INT(11) NOT NULL AUTO_INCREMENT,
+		name VARCHAR(50) NOT NULL DEFAULT '',
+		email VARCHAR(50) NOT NULL DEFAULT '',
+		url VARCHAR(50) NULL DEFAULT 'url',
+		msg TEXT,
+		ip VARCHAR(15) NOT NULL DEFAULT '',
+		browser VARCHAR(50) NOT NULL DEFAULT '',
+		PRIMARY KEY (id)
+		)";
+mysql_query($sql) or die(mysql_error());
+mysql_close();
+
+print '<p>Структура базы данных успешно создана!</p>';
